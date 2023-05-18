e3750998f2fa3772d34fa26c62fc1b4ca3e21979
Cheslyn Herman <cheslynherman1169@gmail.com>
2023-05-18 16:33:41 +0200

    more exercises added

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-let name= 'Simon';
-console.log(name);
+// let name= 'Simon';
+// console.log(name);
 
-let number= 'Simon';
-console.log(number);
-// when you console log it gives the value of simon for both name and number variables
+// let number= 'Simon';
+// console.log(number);
+// // when you console log it gives the value of simon for both name and number variables
 
-let person= "jason";
-console.log(typeof person);
+// let person= "jason";
+// console.log(typeof person);
 
 let pin= 5;
 console.log(typeof pin);
@@ -19,3 +19,30 @@ console.log(typeof status);
 
 let gender= null;
 console.log(typeof gender);
+
+let person= {
+    name: 'John',
+    age: 30
+};
+console.log(person);
+
+let person2= {
+    name: "Greg",
+    age:12
+};
+console.log(person2);
+
+let people= [person, person2];
+console.log(people);
+
+let first_number=15;
+let second_number=17;
+// temp=first_number;
+// first_number=second_number;
+// second_number=temp;
+
+// first_number=first_number + second_number;
+// second_number=first_number - second_number;
+// first_number=first_number - second_number;
+
+ [first_number, second_number]=[second_number, first_number];
