1fce602abcb2fe0df29ecb8c3b425d93283ca722
Cheslyn Herman <cheslynherman1169@gmail.com>
2023-05-18 14:03:43 +0200

    exercises

---
+++ index.js
@@ -0,0 +1,21 @@
+let name= 'Simon';
+console.log(name);
+
+let number= 'Simon';
+console.log(number);
+// when you console log it gives the value of simon for both name and number variables
+
+let person= "jason";
+console.log(typeof person);
+
+let pin= 5;
+console.log(typeof pin);
+
+let user= undefined;
+console.log(typeof user);
+
+let status= true;
+console.log(typeof status);
+
+let gender= null;
+console.log(typeof gender);
